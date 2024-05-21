872f30dfeb83064b6e891e42a7338695e0b6e3da
abir13719 <abir13719@gmail.com>
2024-05-21 22:28:41 +0600

    Removed console

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8que7r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// const uri = "mongodb://localhost:27017";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,22 +21,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const jobsCollection = client.db("jonNest").collection("allJobs");
-    const appliedJobsCollection = client.db("jonNest").collection("appliedJobs");
-    const SliderCollection = client.db("jonNest").collection("sliders");
-    const ReviewCollection = client.db("jonNest").collection("feedBack");
+    const jobNest = client.db("jonNest");
+    const jobsCollection = jobNest.collection("allJobs");
+    const appliedJobsCollection = jobNest.collection("appliedJobs");
+    const SliderCollection = jobNest.collection("sliders");
+    const ReviewCollection = jobNest.collection("feedBack");
 
     // Read Slider Data
     app.get("/sliders", async (req, res) => {
-      const cursor = SliderCollection.find();
-      const result = await cursor.toArray();
+      const result = await SliderCollection.find().toArray();
       res.json(result);
     });
 
     // Read User Reviews
     app.get("/feedback", async (req, res) => {
-      const cursor = ReviewCollection.find();
-      const result = await cursor.toArray();
+      const result = await ReviewCollection.find().toArray();
       res.json(result);
     });
 
@@ -45,7 +43,7 @@ async function run() {
     app.get("/jobs", async (req, res) => {
       let query = {};
       if (req.query?.postByEmail) {
-        query = { postByEmail: req.query.postByEmail };
+        query.postByEmail = req.query.postByEmail;
       }
       if (req.query?.title) {
         query.title = { $regex: new RegExp(req.query.title, "i") };
@@ -57,52 +55,30 @@ async function run() {
     // Read Single Job Data by ID
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-
-      // Validate the ID
-      if (!ObjectId.isValid(id)) {
-        return res.status(400).json({ error: "Invalid job ID format" });
-      }
-
-      try {
-        const option = {
-          projection: {
-            _id: 1,
-            title: 1,
-            pictureUrl: 1,
-            description: 1,
-            salaryRange: 1,
-            applicantsNumber: 1,
-            category: 1,
-            postingDate: 1,
-            applicationDeadline: 1,
-            postByEmail: 1, // Add this field
-          },
-        };
-        const result = await jobsCollection.findOne({ _id: new ObjectId(id) }, option);
-
-        if (!result) {
-          return res.status(404).json({ error: "Job not found" });
-        }
-
-        res.json(result);
-      } catch (error) {
-        console.error("Error fetching job details:", error);
-        res.status(500).json({ error: "An error occurred while fetching job details" });
-      }
+      const option = {
+        projection: {
+          _id: 1,
+          title: 1,
+          pictureUrl: 1,
+          description: 1,
+          salaryRange: 1,
+          applicantsNumber: 1,
+          category: 1,
+          postingDate: 1,
+          applicationDeadline: 1,
+          postByEmail: 1,
+        },
+      };
+      const result = await jobsCollection.findOne({ _id: new ObjectId(id) }, option);
+      res.json(result);
     });
 
+    // Read Single Job Data by ID and Email
     app.get("/applied/:jobId/:email", async (req, res) => {
       const { jobId, email } = req.params;
-
-      try {
-        const query = { jobId, email }; // jobId should be a string here
-        const appliedJob = await appliedJobsCollection.findOne(query);
-
-        res.json({ applied: !!appliedJob });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
+      const query = { jobId, email };
+      const appliedJob = await appliedJobsCollection.findOne(query);
+      res.json({ applied: !!appliedJob });
     });
 
     // Read Single Job Data by ID
@@ -116,13 +92,12 @@ async function run() {
     // Read applied jobs
     app.get("/applied", async (req, res) => {
       let query = {};
-      if (req.query?.category) {
-        query = { category: req.query.category };
-      }
       if (req.query?.email) {
-        query = { email: req.query.email };
+        query.email = req.query.email;
       }
-      console.log(req.query.email);
+      if (req.query?.category) {
+        query.category = req.query.category;
+      }
       const result = await appliedJobsCollection.find(query).toArray();
       res.json(result);
     });
@@ -130,6 +105,7 @@ async function run() {
     // Create all Jobs
     app.post("/jobs", async (req, res) => {
       const job = req.body;
+      job.applicantsNumber = parseInt(job.applicantsNumber);
       const result = await jobsCollection.insertOne(job);
       res.json(result);
     });
@@ -138,6 +114,8 @@ async function run() {
     app.post("/applied", async (req, res) => {
       const userInfo = req.body;
       const result = await appliedJobsCollection.insertOne(userInfo);
+      const jobId = ObjectId.createFromHexString(userInfo.jobId);
+      await jobsCollection.updateOne({ _id: jobId }, { $inc: { applicantsNumber: 1 } });
       res.json(result);
     });
 
@@ -145,8 +123,7 @@ async function run() {
     app.put("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const updatedJob = req.body;
-
-      // //updating in database
+      updatedJob.applicantsNumber = parseInt(updatedJob.applicantsNumber);
       const filter = { _id: new ObjectId(id) };
       const option = { upsert: true };
       const newUpdatedInformations = {
